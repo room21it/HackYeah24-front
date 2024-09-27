27754f7c349b0fc3419768cd0b5bffd27192d102
Andrzej Pacholik <andrzej.pacholik@gmail.com>
2024-09-27 14:43:53 +0200

    change footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,14 +3,11 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import IconButton from "@mui/material/IconButton";
 import Link from "@mui/material/Link";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 
-import FacebookIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import TwitterIcon from "@mui/icons-material/X";
+import GitHub from "@mui/icons-material/GitHub";
 import Divider from "@mui/material/Divider";
 import { LinkList } from "./LinksList/LinkList";
 import { ORG_NAME } from "~/config/organization";
@@ -24,7 +21,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" mt={1}>
       {"Copyright © "}
-      <Link href="/">{ ORG_NAME }&nbsp;</Link>
+      <Link href="/">{ORG_NAME}&nbsp;</Link>
       {new Date().getFullYear()}
     </Typography>
   );
@@ -96,15 +93,15 @@ export const Footer = () => {
                   color: "text.secondary",
                 }}
               >
-                <IconButton color="inherit" href="https://github.com/mui" aria-label="GitHub" sx={{ alignSelf: "center" }}>
-                  <FacebookIcon />
-                </IconButton>
-                <IconButton color="inherit" href="https://x.com/MaterialUI" aria-label="X" sx={{ alignSelf: "center" }}>
-                  <TwitterIcon />
-                </IconButton>
-                <IconButton color="inherit" href="https://www.linkedin.com/company/mui/" aria-label="LinkedIn" sx={{ alignSelf: "center" }}>
-                  <LinkedInIcon />
-                </IconButton>
+                <Stack direction="row" spacing={2}>
+                  <GitHub />
+                  <Link target="_blank" href="https://github.com/room21it/HackYeah24-front">
+                    Frontend
+                  </Link>
+                  <Link target="_blank" href="https://github.com/room21it/HackYeah24-back">
+                    Backend
+                  </Link>
+                </Stack>
               </Stack>
             </Stack>
           </Box>
