857890b493a3e9baebd30d42ebc2562759004a82
Andrzej Pacholik <andrzej.pacholik@gmail.com>
2024-09-29 10:09:34 +0200

    fix translation of allergens

--- src/app/api/allergens/allergens.ts
+++ src/app/api/allergens/allergens.ts
@@ -1,63 +1,65 @@
 export type Allergens = {
-    name: string;
-    id: string
-}
+  name: string;
+  id: string;
+};
 
 export const listOfAllergens: Allergens[] = [
-    {
-        name: "gluten",
-        id: "gluten"
-    },
-    {
-        name: "orzechy",
-        id: "nuts"
-    },
-    {
-        name: "seler",
-        id: "celery"
-    },
-    {
-        name: "skorupiaki",
-        id: "crustaceans"
-    },
-    {
-        name: "jajka",
-        id: "egg"
-    },
-    {
-        name: "ryby",
-        id: "fish"
-    },
-    {
-        name: "łubin",
-        id: "lupin"
-    },
-    {
-        name: "mleko",
-        id: "milk"
-    },
-    {
-        name: "mięczaki",
-        id: "molluscs"
-    },
-    {
-        name: "gorczyca",
-        id: "mustard"
-    },
-    {
-        name: "orzeszki ziemne",
-        id: "peanuts"
-    },
-    {
-        name: "nasiona sezamu",
-        id: "sesame"
-    },
-    {
-        name: "soja",
-        id: "soya"
-    },
-    {
-        name: "dwutlenek siarki",
-        id: "sulphur"
-    },
-]
+  {
+    name: "gluten",
+    id: "gluten",
+  },
+  {
+    name: "orzechy",
+    id: "nuts",
+  },
+  {
+    name: "seler",
+    id: "celery",
+  },
+  {
+    name: "skorupiaki",
+    id: "crustaceans",
+  },
+  {
+    name: "jajka",
+    id: "egg",
+  },
+  {
+    name: "ryby",
+    id: "fish",
+  },
+  {
+    name: "łubin",
+    id: "lupin",
+  },
+  {
+    name: "mleko",
+    id: "milk",
+  },
+  {
+    name: "mięczaki",
+    id: "molluscs",
+  },
+  {
+    name: "gorczyca",
+    id: "mustard",
+  },
+  {
+    name: "orzeszki ziemne",
+    id: "peanuts",
+  },
+  {
+    name: "nasiona sezamu",
+    id: "sesame",
+  },
+  {
+    name: "soja",
+    id: "soya",
+  },
+  {
+    name: "dwutlenek siarki",
+    id: "sulphur",
+  },
+];
+
+export const allergensMap = new Map(listOfAllergens.map(({ id, name }) => [id, name]));
--- src/app/recipe/[guid]/components/AllergensPanel.tsx
+++ src/app/recipe/[guid]/components/AllergensPanel.tsx
@@ -2,6 +2,7 @@ import Box from "@mui/material/Box";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
+import { allergensMap } from "~/app/api/allergens/allergens";
 import useRecipe from "~/hooks/useRecipe";
 import { useTabPanel } from "~/hooks/useTabPanel";
 
@@ -24,7 +25,7 @@ const AllergensPanel = ({ index, value, guid }: Props) => {
       <List>
         {recipe?.allergens?.map((alergen) => (
           <ListItem key={alergen.id}>
-            <ListItemText primary={alergen.allergenType} />
+            <ListItemText primary={allergensMap.get(alergen.allergenType as string) || alergen.allergenType} />
           </ListItem>
         ))}
       </List>
